a3e96f02137fbe3cbd1321b508637da43e8929fd
João Pedro <jpedrodss@gmail.com>
2020-06-03 10:03:09 -0300

    projeto criado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula09Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
