3cb4dacd9b4a1289291a1b2e34d248950e064831
João Pedro <jpedrodss@gmail.com>
2020-06-03 11:08:08 -0300

    update

--- Calculadora.cs
+++ Calculadora.cs
@@ -5,16 +5,16 @@ namespace Aula09Calculadora
 
         public double[] num = new double[4];
 
-        public double Soma(){
+        public double Somar(){
             return num[0] + num[1];
         }
-        public double Subtracao(){
+        public double Subtrair(){
             return num[0] - num[1];
         }
-        public double Divisao(){
+        public double Dividir(){
             return num[0] / num[1];
         }
-        public double Multiplicacao(){
+        public double Multiplicar(){
             return num[0] * num[1];
         }
     }
--- CalculadoraCientifica.cs
+++ CalculadoraCientifica.cs
@@ -6,7 +6,7 @@ namespace Aula09Calculadora
         string texto;
 
         public double Media(){
-            return (num[0] + num [5]) / num.Length;
+            return (num[0] + num [4]) / num.Length;
         }
     }
 }
